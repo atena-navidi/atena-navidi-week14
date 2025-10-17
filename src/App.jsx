b285a71a87bf1193217b1176821c6750744796d3
Atena Navidi <atenanavidii@gmail.com>
2025-10-17 19:41:51 +0330

    Basic components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
-import './App.css'
+import Contacts from "./components/Contacts.jsx"
+import Header from "./components/Header.jsx"
 
 function App() {
 
   return (
     <>
-    
+      <Header />
+      <Contacts />
     </>
   )
 }
 
 export default App
+
